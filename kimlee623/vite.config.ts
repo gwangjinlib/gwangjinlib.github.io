fc1c4efd056edca215f581f791af66a4bc805e96
Eunkwang Choi <126881223+eunkwangchoi@users.noreply.github.com>
2026-08-14 11:14:13 +0900

    Build kimlee623 game for GitHub Pages

--- kimlee623/vite.config.ts
+++ kimlee623/vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/kimlee623/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
